b2dd491991a073c83546a50c9f06acc9d4bd25a1
Antoine Cheron <cheron.antoine@gmail.com>
2020-08-31 16:11:20 +0200

    Add Open Api documentation and serve it from OPTIONS /

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const path = require('path')
+const YAML = require('yamljs')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const intentRoute = require('./routes/intent')
 require('dotenv').config()
 
+const apiDocumentation = YAML.load(path.join(__dirname, '/openapi.yaml'))
+
 const port = 4000
 
 const app = express()
 
-app.use(cors());
-app.options('*', cors());
+app.use(cors({ preflightContinue: true }))
+app.options('/', (_, res) => res.status(200).json(apiDocumentation))
+app.options('*', cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
