a60e5c540f8d1640ec8d291ef5612635b5f42192
Antoine Cheron <cheron.antoine@gmail.com>
2020-08-31 15:13:41 +0200

    Report cors addition and the set-up of returning a 200 status code for calls to the detectIntent api endpoint from commit 2b8b301 of master

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const intentRoute = require('./routes/intent');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const intentRoute = require('./routes/intent')
+require('dotenv').config()
 
-const port = 4000;
+const port = 4000
 
-const app = express();
+const app = express()
 
 app.use(cors());
 app.options('*', cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+app.get('/', (req, res) => res.send('Hello World!!'))
 
-app.use('/intent', intentRoute);
+app.use('/intent', intentRoute)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
